b75320becfaf592f146d603e74e2621da4a5a79a
ybouhsai <ybouhsai@Ubuntu.myguest.virtualbox.org>
2023-07-25 15:38:34 +0100

    my second commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import tornado.ioloop
 import tornado.web
 
@@ -5,9 +6,26 @@ class HelloWorldHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, World!")
 
+class SumHandler(tornado.web.RequestHandler):
+    def post(self):
+        try:
+            data = json.loads(self.request.body)
+            numbers = data.get("numbers", [])
+            if isinstance(numbers, list):
+                numbers_sum = sum(numbers)
+                self.write({"sum": numbers_sum})
+            else:
+                self.set_status(400)  # Bad Request
+                self.write({"error": "Data must contain a list of numbers."})
+        except json.JSONDecodeError:
+            self.set_status(400)  # Bad Request
+            self.write({"error": "Invalid JSON format."})
+        
+
 def make_app():
     return tornado.web.Application([
         (r"/", HelloWorldHandler),
+        (r"/sum", SumHandler)
     ])
 
 if __name__ == "__main__":

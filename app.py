db51de9e1dcb5b4506adad27c3399607ebd8ccf7
ybouhsai <ybouhsai@Ubuntu.myguest.virtualbox.org>
2023-07-25 19:17:46 +0100

    adding an html page as an interface

--- app.py
+++ app.py
@@ -20,13 +20,38 @@ class SumHandler(tornado.web.RequestHandler):
         except json.JSONDecodeError:
             self.set_status(400)  # Bad Request
             self.write({"error": "Invalid JSON format."})
+
+class CalculatorHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html", result=None, num1=None, num2=None, operation=None)
+    def post(self):
+        operation = self.get_argument("operation")
+        num1 = float(self.get_argument("num1"))
+        num2 = float(self.get_argument("num2"))
+
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "divide":
+            result = num1 / num2
+        elif operation == "subtract":
+            result = num1 - num2
+        else:
+            self.set_status(400)  
+            self.write({"error": "Invalid operation."})
+            return 
+        print("Result :", result)
+        self.render("index.html", result=result, num1=num1, num2=num2, operation=operation)
         
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", HelloWorldHandler),
-        (r"/sum", SumHandler)
-    ])
+        (r"/sum", SumHandler),
+        (r"/calculator", CalculatorHandler)
+    ], template_path="./templates")
 
 if __name__ == "__main__":
     app = make_app()
